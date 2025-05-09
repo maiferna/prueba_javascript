aaf41fb873453546e6b7888afa6228742670b0e8
maiferna <maiderfernandez57@gmail.com>
2025-05-09 09:30:45 +0200

    Add: second exercise

--- js/main.js
+++ js/main.js
@@ -3,9 +3,21 @@
 
 // Ejercicio 1: Devolver caracteres en mayúsculas y separados por espacios.
 
-let nombre = "Ana";
+let string = "Ana";
 
 const modificarPalabra = (palabra) => palabra.toUpperCase().split('').join(' ');
 
-console.log(modificarPalabra(nombre));
+console.log(modificarPalabra(string));
+
+
+
+// Ejercicio 2: Devolver el número de palabras que tiene un nombre
+
+let nombre = "Antonio Alberto Jesús";
+
+const contarPalabras = (palabra) => {
+    return palabra.split(' ').length;
+}
+
+console.log(contarPalabras(nombre));
 
