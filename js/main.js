1b01a89bffb3fcafc285859d20685fbde6cd598a
maiferna <maiderfernandez57@gmail.com>
2025-05-09 09:24:51 +0200

    Add: first exercise

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // Comentar qué variables hemos utilizado, por qué, etc
+
+
+// Ejercicio 1: Devolver caracteres en mayúsculas y separados por espacios.
+
+let nombre = "Ana";
+
+const modificarPalabra = (palabra) => palabra.toUpperCase().split('').join(' ');
+
+console.log(modificarPalabra(nombre));
+
